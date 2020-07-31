654bdbcf74dfe6ba5eeb1aa10f9d5ea33cc03965
DikshyaLimbu <46103185+DikshyaLimbu@users.noreply.github.com>
2020-07-31 17:15:22 +0545

    Update today.js

--- today.js
+++ today.js
@@ -3,6 +3,10 @@ function now(argument) {
 	var h = today.getHours();
 	var m = today.getMinutes();
 	var s = today.getSeconds();
+	if(h <10 ){h='0'+h;}
+	if(m <10 ) {m='0' + m; }
+	if(s<10){s='0' + s;}
 	var ampm= (h>12)?"AM":PM;
-	console.log(h+":"+m+":"+s+ampm);
+	var date = h+"-"+m+"-"+s+" "+ampm;
 }
+document.write(date);
